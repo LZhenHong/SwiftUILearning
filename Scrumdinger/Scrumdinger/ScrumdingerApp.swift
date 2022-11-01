e029d066898fb87d31aba90caed1e4f0f741e1a9
Eden <levine5blue@gmail.com>
2022-11-01 15:01:39 +0800

    [ADD] Apple tutorial project.

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by LZhenHong on 2022/11/1.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
