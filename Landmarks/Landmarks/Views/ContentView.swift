1cd1649172032ce54d571daf50a899b655490841
Eden <levine5blue@gmail.com>
2022-11-09 16:18:07 +0800

    [UPDATE] Build list.

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Eden on 2022/11/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
