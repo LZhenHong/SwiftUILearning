b768dc15791c5fb6dda17b0db7a75e2f829f45b3
Eden <levine5blue@gmail.com>
2022-11-09 20:38:28 +0800

    [ADD] State data.

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -8,13 +8,26 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoriteOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            !showFavoriteOnly || landmark.isFavorite
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoriteOnly) {
+                    Text("Favorite only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
@@ -24,10 +37,6 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (3rd generation)", "iPhone 14 Pro Max"], id: \.self) { deviceName in
-                LandmarkList()
-                    .previewDevice(PreviewDevice(rawValue: deviceName))
-                    .previewDisplayName(deviceName)
-        }
+        LandmarkList()
     }
 }
